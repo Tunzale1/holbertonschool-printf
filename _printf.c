df64be8a6f37aee64c7b8a46e5bf836c30743927
seyyar95 <heydrovsyyr@gmail.com>
2023-11-28 04:31:50 -0800

    printf function is okay

---
+++ _printf.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+#include <stdarg.h>
+#include "main.h"
+int _printf(const char *format, ...)
+{
+    va_list args;
+    
+    
+    
+
+ int i = 0;
+ int count = 0;
+ int c;
+ char *str;
+
+ va_start(args, format);
+   
+    while (format[i] != '\0')
+    {
+        if (format[i] == '%')
+        {
+            i++;
+            switch(format[i])
+            {
+                case 'c':
+                        c = va_arg(args, int);
+                        write(1, &c, 1);
+                        count++;
+                        break;
+                case 's':
+			str = va_arg(args, char*);
+                        count += write(1, str, strlen(str));
+                        break;
+                case '%':
+                        write(1, "%", 1);
+                        count++;
+                        break;
+                default:
+                        write(1, "%", 1);
+                        write(1, &format[i], 1);
+                        count += 2;
+            }
+        }
+        else
+        {
+            write(1, &format[i], 1);
+            count ++;
+        }
+        i++;
+    }
+
+    va_end(args);
+
+    return(count);
+}
